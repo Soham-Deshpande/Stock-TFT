d6c7683b7099792b89fcbce9e1748b7c934a13d0
Soham Deshpande <soham.deshpande@challoners.org>
2021-02-25 15:27:49 +0000

    New files

---
+++ GLU.py
@@ -0,0 +1,11 @@
+#Gated Linear Unit
+from Activation_functions import Sigmoid
+
+
+
+# GLU()
+class GLU():
+    def __init__(self):
+        super().__init__()
+
+
---
+++ NN.py
@@ -0,0 +1,11 @@
+#Neural Networks
+
+import numpy as np
+import theano as th
+
+
+#Linear
+class nn():
+    def __int__(self):
+        super().__init__()
+    def
